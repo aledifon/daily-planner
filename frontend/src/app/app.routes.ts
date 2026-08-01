aaa8c08759cfbe207cb885fa5f27a370ce04fa4c
aledifon <aledifon89@gmail.com>
2026-08-01 21:32:50 +0200

    feat(frontend): add authentication route guards

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+import { guestGuard } from './guards/guest.guard';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
     path: 'login',
-
+    canActivate: [guestGuard],
     // Lazy-loads the LoginPage only when the user navigates to /login.
     // The dynamic import returns a Promise containing the module exports,
     // from which the LoginPage component is selected.
@@ -13,6 +20,7 @@ export const routes: Routes = [
   },
   {
     path: 'tasks',
+    canActivate: [authGuard],
     loadComponent: () =>
       import('./pages/tasks/tasks.page')
         .then((m) => m.TasksPage),
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -15,30 +15,46 @@ export class App implements OnInit {
   protected readonly authService = inject(AuthService);
   private readonly router = inject(Router);  
 
-  ngOnInit(): void {
-    if (!this.authService.isAuthenticated()) {
-      this.router.navigateByUrl('/login', {
-        replaceUrl: true
-      });
+  // ngOnInit(): void {
+  //   if (!this.authService.isAuthenticated()) {
+  //     this.router.navigateByUrl('/login', {
+  //       replaceUrl: true
+  //     });
 
+  //     return;
+  //   }
+
+  //   this.authService.getUserInfo().subscribe({
+  //     next: (response) => {
+  //       this.authService.saveAuthenticatedUser(response.user);
+
+  //       this.router.navigateByUrl('/tasks', {
+  //         replaceUrl: true
+  //       });
+  //     },
+
+  //     error: () => {
+  //       this.authService.logout();
+
+  //       this.router.navigateByUrl('/login', {
+  //         replaceUrl: true
+  //       });
+  //     }
+  //   });
+  // }
+
+  ngOnInit(): void {
+    if (!this.authService.isAuthenticated()) {            
       return;
     }
 
     this.authService.getUserInfo().subscribe({
       next: (response) => {
-        this.authService.saveAuthenticatedUser(response.user);
-
-        this.router.navigateByUrl('/tasks', {
-          replaceUrl: true
-        });
+        this.authService.saveAuthenticatedUser(response.user);        
       },
 
       error: () => {
         this.authService.logout();
-
-        this.router.navigateByUrl('/login', {
-          replaceUrl: true
-        });
       }
     });
   }
